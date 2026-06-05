79693f137a4ffd6a68448dafe80982e4e0e8871b
BaileyShoemake123 <bailey.shoemake@jpschools.org>
2026-06-05 16:25:32 +0000

    worked on dataShapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
   // TODO 2: add a new property to all data shapes
 
   for (var i = 0; i < dataShapes.length; i ++){
-    var currentShape = goodBehavior = "";{
+    var currentShape = goodBehavior;{
       if (dataShapes[i].color === "red"){
         goodBehavior = "bounce";
       } else if (dataShapes[i].color === "blue"){
