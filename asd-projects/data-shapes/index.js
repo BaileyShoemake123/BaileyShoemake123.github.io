713d954eb56b46d370b3402932302801bcc4bc84
BaileyShoemake123 <bailey.shoemake@jpschools.org>
2026-06-08 15:11:12 +0000

    completed data shapes project

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -60,12 +60,26 @@ $(document).ready(function () {
   
 
   // TODO 3-a: add a function that handles the static display type
-  
+  function handleStatic() {
+    setBackgroundWithObject(data);
+    animationDetails.displayType = 1
+
+  }
 
   // TODO 4-a: add a function that handles the good display type
-  
+  function handleGood(color, shape, repeat) {
+    setBackgroundWithSimple(color, shape, repeat);
+    animationDetails.displayType = 2;
+    
+  }
 
   // TODO 5-a: add a function that handles the bad display type
+  function handleBad(data, repeat) {
+    repeat += 1;
+    setBackgroundWithMixed(data, repeat)
+    animationDetails.displayType = 3;
+    
+  }
   
 
   /////////////////////////////////////////////////
@@ -74,17 +88,20 @@ $(document).ready(function () {
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-    
+    handleStatic(dataShapes[currentIndex]);
   }
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+    let currentShape = dataShapes[currentIndex];
+    handleGood(currentShape.color, currentShape.shape, currentShape.repeat);
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+    let currentShape = dataShapes[currentIndex];
+    let repeat = currentShape.repeat;
+    handleBad(currentShape, repeat);
   }
 
   /////////////////////////////////////////////////
