b696ddc37b04e9d52cc49543c50b9c916417bafa
BaileyShoemake123 <bailey.shoemake@jpschools.org>
2026-06-11 16:28:42 +0000

    Save dice app changes

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,4 +1,20 @@
 $(document).ready(function () {
   // Your code goes here
-
+$("<div>")
+  .css("height", 15)
+  .css("width", 15)
+  .css("background-color", "black")
+  .css("position", "absolute")
+  .css("top", 50)
+  .css("left", 50)
+  .appendTo("#die");
 });
+
+function rollDie(){
+  var randomNum = Math.ceil(Math.random() * 6);
+  console.log(randomNum);
+}
+function handleClick(){
+  $("#die").click(rollDie);
+}
+  $("#die").on("click", handleClick);
