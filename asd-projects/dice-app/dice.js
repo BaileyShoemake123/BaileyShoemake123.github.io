6032c3324c184d3f5e7482d72a9a88fd6a449327
BaileyShoemake123 <bailey.shoemake@jpschools.org>
2026-06-16 15:52:26 +0000

    Save progress

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -9,6 +9,18 @@ $("<div>")
   .css("left", 50)
   .appendTo("#die");
 });
+function makeDot(top, left, elementID){
+  var $dot = $("<div>")
+  $dot.addClass("dot");
+  $dot.css ("top", top);
+  $dot.css ("left", left);
+  };
+  $dot.appendTo(elementID);
+
+}
+makeDot(50, 50, "#die"); // middle middle
+makeDot(25, 25, "#die"); // top left
+makeDot(75, 75, "#die"); // bottom right
 
 function rollDie(){
   var randomNum = Math.ceil(Math.random() * 6);
